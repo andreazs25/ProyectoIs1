5e881706045889e6ef0ad6690e4da919c2fbef83
andreazs25 <sileszeballosandrea22@gmail.com>
2016-03-10 16:06:54 +0100

    Update Tablero.java

--- Tablero.java
+++ Tablero.java
@@ -1,6 +1,5 @@
 package minesweeper3;
 
-
 public class Tablero {
 	// Atributos
 	private Casilla[][] casillas;
@@ -26,185 +25,20 @@ public class Tablero {
 		return casillas[pNumF][pNumC];
 	}
 
-	public void inicializarTablero(){
-		//1.Coloca las minas dependiendo del numero de filas y columnas
-		this.ponerMinas();	
-		//2.Coloca el resto de casillas
-		for (int f=0; f<this.numFilas; f++){
-			for (int c=0; c<this.numColumnas; c++){
-				//Si la casilla no tiene mina
-				if (!(this.casillas[f][c] instanceof CasillaConMina)){
-					//Obtenemos las casillas adyacentes
-					int adj = this.obtenerAdyacentes(f,c);
-					//Si no tiene minas adyacentes
-					if (adj==0){
-						this.casillas[f][c]= FactoriaCasilla.getFactoriaCasilla().crearCasilla(0);
-					}
-					//Si tiene al menos una mina adyacente
-					else{
-						this.casillas[f][c]= FactoriaCasilla.getFactoriaCasilla().crearCasilla(1);
-						((CasillaConNumero)casillas[f][c]).setNumeroCasillasAdj(adj);
-					}
-				}
-			}
-		}
+	public int getNumColumnas() {
+		return numColumnas;
 	}
 
-	// Obtiene el numero de minas adyacentes que tiene una casilla
-	private int obtenerAdyacentes(int f, int c) {
-		int adj = 0;
-
-		if (f == 0) {
-
-			if (casillas[f + 1][c] instanceof CasillaConMina) {
-				adj++;
-			}
-
-			if (c == 0) {
-				if (casillas[f][c + 1] instanceof CasillaConMina) {
-					adj++;
-				}
-				if (casillas[f + 1][c + 1] instanceof CasillaConMina) {
-					adj++;
-				}
-			} else if (c == this.numColumnas - 1) {
-
-				if (casillas[f][c - 1] instanceof CasillaConMina) {
-					adj++;
-				}
-				if (casillas[f + 1][c - 1] instanceof CasillaConMina) {
-					adj++;
-				}
-			} else {
-				if (casillas[f][c + 1] instanceof CasillaConMina) {
-					adj++;
-				}
-				if (casillas[f + 1][c + 1] instanceof CasillaConMina) {
-					adj++;
-				}
-				if (casillas[f][c - 1] instanceof CasillaConMina) {
-					adj++;
-				}
-				if (casillas[f + 1][c - 1] instanceof CasillaConMina) {
-					adj++;
-				}
-			}
-		}
-
-		else if (f == this.numFilas - 1) {
-
-			if (casillas[f - 1][c] instanceof CasillaConMina) {
-				adj++;
-			}
-
-			if (c == 0) {
-				if (casillas[f - 1][c + 1] instanceof CasillaConMina) {
-					adj++;
-				}
-				if (casillas[f][c + 1] instanceof CasillaConMina) {
-					adj++;
-				}
-			} else if (c == this.numColumnas - 1) {
-
-				if (casillas[f - 1][c - 1] instanceof CasillaConMina) {
-					adj++;
-				}
-				if (casillas[f][c - 1] instanceof CasillaConMina) {
-					adj++;
-				}
-			} else {
-				if (casillas[f - 1][c + 1] instanceof CasillaConMina) {
-					adj++;
-				}
-				if (casillas[f][c + 1] instanceof CasillaConMina) {
-					adj++;
-				}
-				if (casillas[f - 1][c - 1] instanceof CasillaConMina) {
-					adj++;
-				}
-				if (casillas[f][c - 1] instanceof CasillaConMina) {
-					adj++;
-				}
-			}
-		}
-
-		else if (c == 0) {
-
-			if (casillas[f][c + 1] instanceof CasillaConMina) {
-				adj++;
-			}
-			if (casillas[f - 1][c] instanceof CasillaConMina) {
-				adj++;
-			}
-			if (casillas[f - 1][c + 1] instanceof CasillaConMina) {
-				adj++;
-			}
-			if (casillas[f + 1][c] instanceof CasillaConMina) {
-				adj++;
-			}
-			if (casillas[f + 1][c + 1] instanceof CasillaConMina) {
-				adj++;
-			}
-		}
-
-		else if (c == this.numColumnas - 1) {
-
-			if (casillas[f - 1][c] instanceof CasillaConMina) {
-				adj++;
-			}
-			if (casillas[f - 1][c - 1] instanceof CasillaConMina) {
-				adj++;
-			}
-			if (casillas[f][c - 1] instanceof CasillaConMina) {
-				adj++;
-			}
-			if (casillas[f + 1][c - 1] instanceof CasillaConMina) {
-				adj++;
-			}
-			if (casillas[f + 1][c] instanceof CasillaConMina) {
-				adj++;
-			}
-		} else {
-			if (casillas[f - 1][c - 1] instanceof CasillaConMina) {
-				adj++;
-			}
-			if (casillas[f][c - 1] instanceof CasillaConMina) {
-				adj++;
-			}
-			if (casillas[f + 1][c - 1] instanceof CasillaConMina) {
-				adj++;
-			}
-			if (casillas[f - 1][c] instanceof CasillaConMina) {
-				adj++;
-			}
-			if (casillas[f + 1][c] instanceof CasillaConMina) {
-				adj++;
-			}
-			if (casillas[f - 1][c + 1] instanceof CasillaConMina) {
-				adj++;
-			}
-			if (casillas[f][c + 1] instanceof CasillaConMina) {
-				adj++;
-			}
-			if (casillas[f + 1][c + 1] instanceof CasillaConMina) {
-				adj++;
-			}
-		}
-		return adj;
+	public int getNumFilas() {
+		return numFilas;
 	}
 
-	// Coloca minas en x lugares random independientemente del nivel
-	private void ponerMinas() {
-		for (int i = 0; i < this.numMinas; i++) {
-			int fila = ((int) (Math.random() * this.numFilas));
-			int columna = ((int) (Math.random() * this.numColumnas));
+	public int getNumMinas() {
+		return numMinas;
+	}
 
-			if (this.casillas[fila][columna] instanceof CasillaConMina) {
-				i = i - 1;
-			} else {
-				this.casillas[fila][columna] = FactoriaCasilla.getFactoriaCasilla().crearCasilla(2);
-			}
-		}
+	public Casilla[][] getTablero() {
+		return casillas;
 	}
 
 	// En caso de descubrir casilla vacía, descubriremos las casillas vacías
@@ -237,31 +71,34 @@ public class Tablero {
 	// descubiertas
 	private boolean todasDescubiertas() {
 		boolean todas = false;
-		if ((this.numFilas*this.numColumnas) == this.numDesveladas + this.numMinas) {
+		if ((this.numFilas * this.numColumnas) == this.numDesveladas
+				+ this.numMinas) {
 			todas = true;
 		}
 		return todas;
 	}
-	
-	public void graficar(){
-		
-		for (int f=0; f<this.numFilas; f++){
+
+	public void graficar() {
+
+		for (int f = 0; f < this.numFilas; f++) {
 			System.out.println("");
-			for (int c=0; c<this.numColumnas; c++){
-				System.out.print("["+(f+1)+","+(c+1)+"]");
-				if(casillas[f][c] instanceof CasillaConMina){
-				System.out.print("-*-");
+			for (int c = 0; c < this.numColumnas; c++) {
+				System.out.print("[" + (f + 1) + "," + (c + 1) + "]");
+				if (casillas[f][c] instanceof CasillaConMina) {
+					System.out.print("-*-");
 				}
-				if(casillas[f][c] instanceof CasillaVacia){
-				System.out.print("-0-");
+				if (casillas[f][c] instanceof CasillaVacia) {
+					System.out.print("-0-");
 				}
-				if(casillas[f][c] instanceof CasillaConNumero){
-				System.out.print("-"+((CasillaConNumero) casillas[f][c]).getNumeroCasillasAdj()+"-");
+				if (casillas[f][c] instanceof CasillaConNumero) {
+					System.out.print("-"
+							+ ((CasillaConNumero) casillas[f][c])
+									.getNumeroCasillasAdj() + "-");
 				}
 			}
 		}
 	}
-	
+
 	public void descubrirCasillasAdj1(int f, int c) {
 
 		this.descubrirCasillasAdj(f, c);
@@ -303,13 +140,6 @@ public class Tablero {
 
 	}
 
-	
-	
-	
-	
-	
-	
-
 	/**
 	 * //En caso de descubrir todas las casillas que no son minas, ganamos
 	 * public void finExito(){ System.out.print("Has ganado"); }
@@ -318,3 +148,4 @@ public class Tablero {
 	 * finFracaso(){ System.out.print("Has perdido"); }
 	 **/
 }
+
