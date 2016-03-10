a5a1a1ecc8f7a67f520ee8d68dea403b944ba0e2
ivansalazar94 <ivansalazar94@gmail.com>
2016-03-10 15:53:44 +0100

    Update BuilderTablero3.java
    Quitar el parámetro tablero

--- BuilderTablero3.java
+++ BuilderTablero3.java
@@ -1,12 +1,13 @@
 package minesweeper3;
 
 public class BuilderTablero3 extends BuilderTablero {
-
+	
 	public BuilderTablero3() {
 	}
 
 	@Override
-	public void buildDimensionesTablero(Tablero tablero) {
-		tablero.setDimensionesTablero(12, 25, 75);
+	public void buildDimensionesTablero() {
+		super.tablero.setDimensionesTablero(12, 25, 75);
+		
 	}
 }
